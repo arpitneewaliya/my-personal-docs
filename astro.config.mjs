2b71098edc2a16f106402c72fc49a1208aab044c
Arpit Neewaliya <arpit.neewaliya@gmail.com>
2026-03-20 19:16:29 +0530

    Update Astro configuration and documentation: changed site title to 'MyDocs', added 'Git Basics' section, removed unused image, and updated hero section with new tagline and image.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'MyDocs',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
@@ -19,6 +19,12 @@ export default defineConfig({
 				{
 					label: 'Reference',
 					autogenerate: { directory: 'reference' },
+				},
+				{
+					label: 'Git Basics',
+					items: [
+						{ label: 'Commands', slug: 'git-basics/commands' },
+					],
 				},
 			],
 		}),
